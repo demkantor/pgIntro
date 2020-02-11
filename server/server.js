bcdd00efbaf4f5357b499fdebf8fe5c271092e74
demkantor <demkantor@gmail.com>
2020-02-11 16:28:56 -0600

    added db

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const pg = require('pg');
 
 // uses
 app.use(express.static('server/public'));
@@ -10,10 +11,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 // globals
 const port = 5000;
 
-
 // db setup
-
-
+const pool = new pg.Pool({
+    database: 'music_library',
+    host: 'localhost',
+    port: 5432,
+    max: 12,
+    idleTimeoutMillis: 30000
+});//end pool
 
 // server up
 app.listen(port, ()=>{
